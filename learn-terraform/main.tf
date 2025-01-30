9713af4c2e805de112babdd758558cb9752ebae0
kenro-t <ngkn1425@gmail.com>
2025-01-30 14:12:29 +0000

    terraform

---
+++ learn-terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  // terraformのバージョンを指定
+  required_version = ">= 1.5.1"
+
+  // awsプロバイダーのバージョンを指定
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+
+// awsプロバイダーの設定
+provider "aws" {
+  // regionを指定
+  region = "us-east-1"
+}
