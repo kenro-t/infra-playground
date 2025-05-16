92e45b168a84cdeb3e8813059dd6fefcde4d5500
kenro-t <ngkn1425@gmail.com>
2025-05-17 00:22:31 +0900

    アウトバウント削除

--- cdk-playground/lib/stack/cdk-playground-stack.ts
+++ cdk-playground/lib/stack/cdk-playground-stack.ts
@@ -49,22 +49,6 @@ export class CdkPlaygroundStack extends cdk.Stack {
       ec2.Port.tcp(80),
       "Allow HTTP traffic from anywhere"
     );
-    securityGroup.addEgressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      "Allow HTTP traffic to anywhere"
-    );
-    securityGroup.addEgressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(443),
-      "Allow HTTPS traffic to anywhere"
-    );
-    securityGroup.addEgressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(22),
-      "Allow SSH traffic to anywhere"
-    );
-
 
     // S3バケットの作成
     const myBucket = new s3.Bucket(this, "MyBucket", {
